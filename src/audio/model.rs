f91bd979e80958c97bee877e1b5d12bd5dde5c31
Austin Riba <austin@m51.io>
2025-11-27 20:06:07 -0800

    Better shuffle implementation using a random seed.

--- src/audio/model.rs
+++ src/audio/model.rs
@@ -4,6 +4,7 @@ use gtk::{
     subclass::prelude::*,
 };
 use log::warn;
+use rand::Rng;
 use std::cell::{Cell, RefCell};
 use std::sync::OnceLock;
 
@@ -103,14 +104,18 @@ impl AudioModel {
         self.load_song(start_index as i32);
         self.play();
         self.notify_mpris_can_navigate(true, start_index > 0);
-        self.generate_shuffle_order();
+        if self.imp().shuffle_enabled.get() {
+            self.new_shuffle_cycle();
+        }
     }
 
     pub fn append_to_queue(&self, songs: Vec<SongModel>) {
         self.imp().queue.borrow_mut().extend(songs);
         let current_index = self.queue_index();
         self.notify_mpris_can_navigate(true, current_index > 0);
-        self.generate_shuffle_order();
+        if self.imp().shuffle_enabled.get() {
+            self.new_shuffle_cycle();
+        }
     }
 
     pub fn play_song(&self, index: usize) {
@@ -235,51 +240,73 @@ impl AudioModel {
 
     pub fn set_shuffle(&self, enabled: bool) {
         self.imp().shuffle_enabled.set(enabled);
-        self.generate_shuffle_order();
+        if enabled {
+            self.new_shuffle_cycle();
+        }
     }
 
-    fn generate_shuffle_order(&self) {
-        let imp = self.imp();
+    fn new_shuffle_cycle(&self) {
+        let new_seed = rand::rng().random::<u64>();
+        self.imp().shuffle_seed.set(new_seed);
+        self.imp().shuffle_index.set(0);
+    }
+
+    fn get_shuffle_order(&self) -> Vec<usize> {
         let queue_len = self.queue().len();
         if queue_len == 0 {
-            return;
+            return Vec::new();
         }
         let mut indicies: Vec<usize> = (0..queue_len).collect();
-        use rand::seq::SliceRandom;
-        indicies.shuffle(&mut rand::rng());
-        let current_song_index = imp.queue_index.get() as usize;
-        if let Some(shuffle_index) = indicies.iter().position(|i| *i == current_song_index) {
-            indicies.swap(shuffle_index, 0);
-        }
-        imp.shuffle_queue.replace(indicies);
-        imp.shuffle_index.set(0);
+        use rand::{SeedableRng, seq::SliceRandom};
+        let mut rng = rand::rngs::StdRng::seed_from_u64(self.imp().shuffle_seed.get());
+        indicies.shuffle(&mut rng);
+        indicies
     }
 
     fn next_shuffled(&self) {
-        let pos = self.imp().shuffle_index.get() + 1;
-        if pos < self.imp().shuffle_queue.borrow().len() {
-            self.imp().shuffle_index.set(pos);
-            let actual_index = self.imp().shuffle_queue.borrow()[pos];
-            self.load_song(actual_index as i32);
+        let shuffle_order = self.get_shuffle_order();
+        let current_pos = self.imp().shuffle_index.get();
+        let next_pos = current_pos + 1;
+
+        if next_pos < shuffle_order.len() {
+            self.imp().shuffle_index.set(next_pos);
+            let song_index = shuffle_order[next_pos];
+            self.load_song(song_index as i32);
             self.play();
         } else {
+            self.new_shuffle_cycle();
             self.load_song(0);
-            self.generate_shuffle_order();
             self.stop();
             self.emit_by_name::<()>("queue-finished", &[]);
         }
     }
 
     fn prev_shuffled(&self) {
-        let prev_index = if self.get_position() > 3 {
-            self.imp().shuffle_index.get()
-        } else {
-            self.imp().shuffle_index.get().saturating_sub(1)
-        };
+        let shuffle_order = self.get_shuffle_order();
+        let current_pos = self.imp().shuffle_index.get();
 
-        self.imp().shuffle_index.set(prev_index);
-        let actual_index = self.imp().shuffle_queue.borrow()[prev_index];
-        self.load_song(actual_index as i32);
+        if self.get_position() > 3 {
+            dbg!("current position");
+            // Restart current song if less then 3 seconds have elapsed
+            if let Some(&song_index) = shuffle_order.get(current_pos) {
+                dbg!(&shuffle_order);
+                dbg!(&current_pos);
+                dbg!(&song_index);
+                self.load_song(song_index as i32);
+            }
+        } else if current_pos > 0 {
+            // Go to previous song
+            let prev_pos = current_pos - 1;
+            self.imp().shuffle_index.set(prev_pos);
+            if let Some(&song_index) = shuffle_order.get(prev_pos) {
+                self.load_song(song_index as i32);
+            }
+        } else {
+            // At start of shuffle - just restart first song
+            if let Some(&song_index) = shuffle_order.first() {
+                self.load_song(song_index as i32);
+            }
+        }
         self.play();
     }
 }
@@ -330,8 +357,8 @@ mod imp {
         pub queue: RefCell<Vec<SongModel>>,
         pub mpris_server: OnceCell<LocalServer<super::AudioModel>>,
         pub shuffle_enabled: Cell<bool>,
-        pub shuffle_queue: RefCell<Vec<usize>>,
         pub shuffle_index: Cell<usize>,
+        pub shuffle_seed: Cell<u64>,
     }
     #[glib::object_subclass]
     impl ObjectSubclass for AudioModel {
