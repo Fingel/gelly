aae094e5ad483809495363db8c40adeb5f7c35cd
Austin Riba <austin@m51.io>
2026-08-20 11:07:26 -0700

    Address rust 1.98 clippy lint about large result error variant in glycin.

--- src/ui/image_utils.rs
+++ src/ui/image_utils.rs
@@ -1,7 +1,7 @@
 use glycin::{ErrorCtx, Loader};
 use gtk::gdk::Texture;
 
-pub async fn bytes_to_texture(image_data: &[u8]) -> Result<Texture, ErrorCtx> {
+pub async fn bytes_to_texture(image_data: &[u8]) -> Result<Texture, Box<ErrorCtx>> {
     let image = Loader::new_vec(image_data.to_vec()).load().await?;
     let texture = image.next_frame().await?.texture();
     Ok(texture)
